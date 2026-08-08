e6a72d5e7ea210316cfabd2cb53f9acef89ac602
elbrkamahel-ops <elbrkamahel@gmail.com>
2026-08-08 13:01:08 +0300

    Create index.php

---
+++ shop/index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "<h1 style='font-family:Arial;text-align:center;margin-top:100px;color:#198754;'>
+مول البركة يعمل بنجاح
+</h1>";
+
+echo "<p style='font-family:Arial;text-align:center;'>
+تم تشغيل صفحة المتجر، وسنبدأ الآن تركيب التصميم الاحترافي.
+</p>";
+?>
